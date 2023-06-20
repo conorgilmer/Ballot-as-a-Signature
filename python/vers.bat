e7cab6e926d05eca769a8cf485ae030aee3c48c3
Conor Gilmer <conor.gilmer@gmail.com>
2023-06-20 18:53:04 +0100

    tidy up post programmes which were missed

---
+++ python/vers.bat
@@ -0,0 +1,13 @@
+echo " VERSIONS "
+javac -version
+conda -V
+python --version
+perl -version | grep 'This is'
+cobc -version | grep 'cobc'
+fpc -v | grep 'Compiler'
+lua -v
+awk -version
+ant -version
+make -v | grep 'Make'
+conda --version
+jupyter --version
