dbcd9306422e414ae5cbe537cdb6666a53e129bf
Conor Gilmer <conor.gilmer@gmail.com>
2023-06-20 17:50:45 +0100

    tidy up post programmes which were missed

--- python/QuotaCalc.py
+++ python/QuotaCalc.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Quota(Q) in a PR-STV Election
+# V = total number of votes
+# S = number of seats to allocate
+# The Droop Quota is
+# Q = (V/S+1) + 1
+# The Hare Quota is
+# Q = (V/S)
 
 import math
+
+#Check an integer has been inputted
 def check_user_input(input):
     try:
         # Convert it into integer
@@ -11,9 +20,12 @@ def check_user_input(input):
     except ValueError:
         print("No.. input is not a number. It's a string")
 
+#Main Programm
 V = input("Enter in the valid number of votes ")
 v = check_user_input(V)
 S = input("Enter in the number of seats to allocate ")
 s = check_user_input(S)
-Q =(v/s+1)
-print("the Quota is ",Q) 
+Q = (v/s)
+print("the Hare Quota is ",Q) 
+Q = round((v/(s+1))+1)
+print("the Droop Quota is ",Q) 
