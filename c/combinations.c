7a97b75b13201b3eb8e54c7677a523f40bf2f55f
Conor Gilmer <conor.gilmer@gmail.com>
2023-06-22 09:30:23 +0100

    tidy

---
+++ c/combinations.c
@@ -0,0 +1,38 @@
+//Program to find the number of combinations nCr of a given number
+//nCr = ((n!)/(n-r)!)r!
+
+
+#include<stdio.h>
+
+
+/* Factorial function */
+int fact(int n){
+int fact = 1, x;
+    for(x=1;x<=n;x++)
+        fact=fact*x; 
+
+    return(fact);
+}
+
+int main(){
+
+    int x,comb,n,r;
+
+    printf("Enter a number for n : ");
+
+    scanf("%d",&n); 
+
+    printf("Enter a number: for r : ");
+
+    scanf("%d",&r); 
+
+
+    comb = (fact(n)/fact(n-r)) * fact(r);
+//    printf("fact (n) %d\n", fact(n));
+//    printf("fact (n-r) %d\n", fact(n-r));
+    printf("Combinations of %dP%d is: %d",n,r,comb);
+
+    printf("\n");
+    return 0;
+
+}
