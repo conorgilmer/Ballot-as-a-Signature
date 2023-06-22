f49386f3ab4b8af8833020c5353319d8695e8b65
Conor Gilmer <conor.gilmer@gmail.com>
2023-06-22 01:26:21 +0100

    tidy up

---
+++ c/fact.c
@@ -0,0 +1,21 @@
+//Program to find the factorial of a given number
+#include<stdio.h>
+
+int main(){
+
+    int x,fact=1,n;
+
+    printf("Enter a number to find factorial: ");
+
+    scanf("%d",&n); 
+
+    for(x=1;x<=n;x++)
+
+        fact=fact*x; 
+
+    printf("Factorial of %d is: %d",n,fact);
+
+    printf("\n");
+    return 0;
+
+}
